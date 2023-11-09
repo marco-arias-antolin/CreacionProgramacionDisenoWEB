71bddc882054bf3552c348c4268930550d6ccfda
Marco Antonio Arias Antolín <marcoariasantolin@gmail.com>
2023-11-09 20:21:56 +0100

    Juguetes 	modified:   Juguetes/css/styleJuguetes.css 	modified:   Juguetes/index.html 	modified:   Juguetes/js/scriptJuguetes.js 	new file:   "\342\204\263\360\235\222\234\342\204\233\360\235\222\246\360\235\222\252.txt"

--- Juguetes/js/scriptJuguetes.js
+++ Juguetes/js/scriptJuguetes.js
@@ -1,13 +1,23 @@
 const sleep = ms => new Promise(r => setTimeout(r, ms));
+let displayBanner = true;
+const banner = document.getElementById('banner');
+const bannerTime = 2;
 
 const bannerDisplay = async () => {
-	let bannerP = document.getElementById('banner').querySelectorAll('p')
-	let displayBanner = true;
-	for (let i = 0; i < bannerP.length; i++) {
-		let lastP = (i - 1 > 0) ? i - 1 : bannerP.length - 1;
-		bannerP[lastP].style.opacity = 0;
-		bannerP[i].style.opacity = 1;
-		await sleep(2 * 1000);
+	let bannerP = banner.querySelectorAll('p');
+	while (displayBanner) {
+		for (let i = 0; i < bannerP.length; i++) {
+			let lastP = (i - 1 >= 0) ? i - 1 : bannerP.length - 1;
+			bannerP[lastP].style.opacity = 0;
+			bannerP[i].style.opacity = 1;
+			await sleep(bannerTime * 1000);
+		}
 	}
 }
 bannerDisplay();
+
+let closeBanner = banner.querySelector('.icon-cross-line');
+closeBanner.onclick = (event) => {
+	banner.style.display = 'none';
+	displayBanner = false;
+}
